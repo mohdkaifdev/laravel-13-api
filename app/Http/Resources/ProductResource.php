e3b8b43289f5fe372edb602d113ca213cf823e9d
mohdkaifdev <officialkaif98@gmail.com>
2026-08-07 19:30:18 +0530

    Fix code style issues

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+
 /**
  * @mixin Product
  */
